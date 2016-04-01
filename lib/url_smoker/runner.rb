a763b94bff69cf5d1dc425f3b21848ddcd5440f5
Jim Cushing <jcushing@groupcse.com>
2016-04-01 16:46:22 -0400

    more consise output

--- lib/url_smoker/runner.rb
+++ lib/url_smoker/runner.rb
@@ -1,18 +1,19 @@
 module UrlSmoker
   def self.run_tests(site)
-    puts "Testing #{site.name} (#{site.base_url}"
+    puts "Testing #{site.name} (#{site.base_url})"
     site.each do |test_case|
       response = Net::HTTP.get_response test_case.uri
       results = test_case.eval response
 
       success = results.all? {|success, _| success}
-      puts "    FAIL".red + ": #{test_case.uri}" unless success
-      puts "    PASS".green + ": #{test_case.uri}" if success
+      puts "FAIL".red + ": #{test_case.uri}" unless success
+      puts "PASS".green + ": #{test_case.uri}" if success
 
-
-      results.each do |success, condition|
-        puts "       FAIL".red + ": #{condition}" unless success
-        puts "       PASS".green + ": #{condition}" if success
+      if !success
+        failed_results = results.select {|success, _| !success }
+        failed_results.each do |success, condition|
+          puts "      #{condition}".red
+        end
       end
 
     end
